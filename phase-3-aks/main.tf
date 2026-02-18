120d5a29dda5667453276a3f9cea9ba656909599
haja <haja@student.chalmers.se>
2026-02-18 09:35:19 +0100

    feat: provision aks cluster with cilium networking

--- phase-3-aks/main.tf
+++ phase-3-aks/main.tf
@@ -17,3 +17,28 @@ resource "azurerm_resource_group" "aks" {
   name     = "adam-azure-cluster"
   location = "North Europe"
 }
+
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "forge-cluster"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = "forge"
+  kubernetes_version  = "1.32.0"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    network_policy     = "cilium"
+    network_data_plane = "cilium"
+  }
+
+}
